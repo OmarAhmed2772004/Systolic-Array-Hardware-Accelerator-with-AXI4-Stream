3953b5fe77471a94c0ea038df21e2f93ec503840
Omar Ahmed <omarahmedabdelaty231@gmail.com>
2026-03-10 12:18:30 +0200

    Refactor mac_pe module to include pipeline enable

--- mac_pe.sv
+++ mac_pe.sv
@@ -1,13 +1,11 @@
-// ---------------------------------------------------------
-// Module: mac_pe
-// ---------------------------------------------------------
+
 module mac_pe #(
     parameter DWIDTH = 8,
     parameter PWIDTH = 16
 )(
     input  logic clk,
     input  logic rst_n,
-    input  logic en,             // <--- NEW: Pipeline Enable
+    input  logic en,             
     input  logic load_weight,
     input  logic [DWIDTH-1:0] weight_in,
     input  logic [DWIDTH-1:0] act_in,
@@ -23,7 +21,7 @@ module mac_pe #(
             weight_reg <= '0;
             act_out    <= '0;
             psum_out   <= '0;
-        end else if (en) begin  // <--- NEW: Freeze state if disabled
+        end else if (en) begin  
             if (load_weight) begin
                 weight_reg <= weight_in;
             end else begin
